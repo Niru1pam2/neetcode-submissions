62f3678abb8487cff74589b87f75de88ae691398
Nirupam Talukdar <nirupamtalukdar123@gmail.com>
2026-07-07 19:58:58 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let res = new Array(temperatures.length).fill(0)
+
+        for (let i = 0; i < temperatures.length - 1; i++) {
+            for (let j = i + 1; j < temperatures.length; j++) {
+                if (temperatures[i] < temperatures[j]) {
+                    res[i] = j - i
+                    break
+                }
+            }
+        }
+
+        return res
+    }
+}
